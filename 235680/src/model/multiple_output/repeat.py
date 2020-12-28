088285654d1af9bd119ec03574eee25c348ddf15
parkjuida <bees1114@naver.com>
2020-12-28 14:34:53 +0900

    Add experiments

--- 235680/src/model/multiple_output/repeat.py
+++ 235680/src/model/multiple_output/repeat.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 
 class RepeatBaseline(tf.keras.Model):
     def call(self, inputs, training=None, mask=None):
-        return inputs
+        return tf.tile(inputs, tf.constant([1, 2, 1], tf.int32))
 
     def get_config(self):
         pass
